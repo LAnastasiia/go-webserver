1c549a1ce723cf64c2efcad96f9ba7b86b1d176a
LAnastasiia <lazorenko@ucu.edu.ua>
2020-07-15 11:56:55 +0300

    storing tls cert/key as secrets

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ set -euo pipefail
 key_dir="$(mktemp -d)"
 ./generate-keys.sh "$key_dir"
 
-kubectl create secret tls webserver-tls \
-    --cert "${key_dir}/tls.crt" \
-    --key "${key_dir}/tls.key"
+kubectl create secret tls tls-secret \
+  --cert "${key_dir}/tls.crt" \
+  --key "${key_dir}/tls.key"
 
 rm -rf "$key_dir"
 
-kubectl create -f -
+kubectl apply -f resources-compose.yaml
